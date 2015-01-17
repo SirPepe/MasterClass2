0d298f354be409204b91db4dead9b31e1b22345f
Peter Kröner <peter@peterkroener.de>
2015-01-17 01:16:40 +0100

    Gluecode umsortiert und mehr kommentiert

--- script/app.js
+++ script/app.js
@@ -4,14 +4,19 @@ require(['jquery', 'q', 'util',
   requestStream, renderVideo, takeScreenshot, resizeImage
 ) {
 
-  $('#Start').on('click', function() {
+  $('#Start').on('click', function(){
+
+
+    /* Schritt 1: Videofeed anzeigen */
 
     // Stream anfragen, als Video einblenden und das
     // Video-Objekt zurückgeben
     var whenVideo = requestStream()
       .then(renderVideo)
-      .then(function(video) {
-        $(video).appendTo('#VideoWrapper').get(0).play();
+      .then(function(video){
+        $(video).appendTo('#VideoWrapper')
+          .get(0)
+          .play();
         $('#Start').attr('disabled', true);
         return video;
       });
@@ -21,29 +26,12 @@ require(['jquery', 'q', 'util',
       util.handleError('Kann Stream nicht anzeigen')
     );
 
-    // Wenn der Videostream bereit ist, wird
-    // der Foto-Button aktiviert
-    whenVideo.then(function(video) {
-      $('#Photo')
-        .attr('disabled', false)
-        .on('click', function() {
-          // Foto aufnehmen und anzeigen
-          var whenPhoto = takePhoto(video);
-          whenPhoto
-            .then(function(photo) {
-              return photo.to$();
-            })
-            .then(function($photo) {
-              return $photo.appendTo('#Gallery');
-            })
-            .fail(
-              util.handleError('Konnte Foto nicht erstellen')
-            );
-        });
-    });
 
-    // Screenshot und Thumbnail erstellen, Photo-Objekt zurückgeben
-    function takePhoto(source) {
+    /* Schritt 2: Foto-Aufnehm-Funktion */
+
+    // Screenshot und Thumbnail erstellen,
+    // Photo-Objekt zurückgeben
+    function takePhoto(source){
       return Q.all([
           takeScreenshot(source),
           resizeImage(source, 100, 100)
@@ -53,8 +41,31 @@ require(['jquery', 'q', 'util',
         });
     }
 
-    // Bilder aus der Galerie löschen
-    $('#Gallery').on('click', 'button', function(evt) {
+    /* Schritt 3: Fotos aufnehmen und anzeigen */
+
+    // Wenn der Videostream bereit ist, wird
+    // der Foto-Button aktiviert
+    whenVideo.then(function(video){
+      $('#Photo')
+        .attr('disabled', false)
+        .on('click', function(){
+          // Foto aufnehmen und anzeigen
+          takePhoto(video)
+            .then(function(photo){
+              return photo.to$();
+            })
+            .then(function($photo){
+              return $photo.appendTo('#Gallery');
+            })
+            .fail(
+              util.handleError('Konnte Foto nicht erstellen')
+            );
+        });
+    });
+
+    /* Schritt 4: Bilder aus der Galerie löschen */
+
+    $('#Gallery').on('click', 'button', function(evt){
       $(evt.target).parent().remove();
     });
 
